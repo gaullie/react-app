6102df8e7b57d0fad1ce018d04fd2ae772360d66
gaullie <paulgoodluck@gmail.com>
2025-08-30 17:15:29 +0100

    I changed this

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//general
+const $ = (param , doc = document) => doc.querySelector(param)
+//menu toggle
 const menuBtn = document.getElementById("menuBtn");
-const ul = document.querySelector("ul");
-
 menuBtn.addEventListener("click", () => {
-    ul.classList.toggle("show");
+    $('#navUl').classList.toggle("show");
 });
+
+// footer year
+const year = document.getElementById('year')
+year.textContent = new Date().getFullYear();
 
